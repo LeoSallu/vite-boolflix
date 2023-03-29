95777bd1e875cb0ce14be768a0525544a5dad226
LeoSallu <L.sallustio99@gmail.com>
2023-03-29 18:03:53 +0200

    funzione per bandiera creata

--- src/App.vue
+++ src/App.vue
@@ -6,17 +6,17 @@ export default {
     data() {
         return {
             store,
-            url: 'https://api.themoviedb.org/3/search/movie?api_key=5c9194b00bb5ab97cca5858e87b81858',
+            movieUrl: 'https://api.themoviedb.org/3/search/movie?api_key=5c9194b00bb5ab97cca5858e87b81858',
             title: '',
             originalTitle: '',
-            language: '',
+            language:'' ,
             score: 0,
             movies: []
         }
     },
     methods: {
         searchMovies() {
-            axios.get(this.url, {
+            axios.get(this.movieUrl, {
                 params: {
                     api_key: this.api_key,
                     query: this.title,
@@ -26,32 +26,49 @@ export default {
                 .then(response => {
                     this.movies = response.data.results;
                 })
+        },
+        flag(element){
+            if(element==="en"){
+                return console.log('Bandiera inglese')
+            }
+            else if (element==="it"){
+                return console.log('Bandiera italiana')
+            }
+            else {
+                console.log('Altro')
+            }
         }
     },
     created() {
+        this.searchMovies()
     }
 }
 </script>
 <!-- My Html -->
 <template>
+    <!-- Form per far digitare il film da cercare  -->
     <form @submit.prevent="searchMovies">
         <input type="text" v-model="title">
         <button type="submit">Search</button>
     </form>
+    <!-- /Form per far digitare il film da cercare  -->
     <div v-if="movies.length">
+        <!-- Ciclo V-for per stampare a schermo le info del film  -->
         <div v-for="movie in movies">
-            <h3>{{ movie.title }}</h3>
+            <h3>Il Titolo del film è : <span>{{title }}</span> .</h3>
             <ul>
                 <li>Il titolo originale del film è : {{ movie.original_title }}</li>
-                <li>La lingua originale del film è : {{ movie.original_language }}</li>
+                <li>La lingua originale del film è : {{flag(movie.original_language)}}</li>
                 <li>Il voto della critica del film è : {{ movie.vote_average }}</li>
             </ul>
         </div>
+        <!-- /Ciclo V-for per stampare a schermo le info del film  -->
     </div>
+    <!-- Messaggio in caso di nessun risultato  -->
     <div v-else>
         <p>Nessun Risultato... prova a cercare qualcos'altro</p>
-        
     </div>
+    <!-- /Messaggio in caso di nessun risultato  -->
 </template>
 <!-- My Scss -->
 <style lang="scss" scoped>
@@ -59,7 +76,7 @@ form {
     display: flex;
 }
 div{
-    h3{
+    span{
         color: pink;
     }
     ul{
