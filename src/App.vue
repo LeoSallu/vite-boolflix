97d6780fa85b015ac5b37d366e4d9465e2c63e45
LeoSallu <L.sallustio99@gmail.com>
2023-03-29 16:44:22 +0200

    implementata ricerca + info film

--- src/App.vue
+++ src/App.vue
@@ -6,27 +6,45 @@ export default {
     data() {
         return {
             store,
+            url: 'https://api.themoviedb.org/3/search/movie?api_key=5c9194b00bb5ab97cca5858e87b81858',
             title: '',
             originalTitle: '',
             language: '',
-            score: 0
+            score: 0,
+            movies: []
+        }
+    },
+    methods: {
+        searchMovies() {
+            axios.get(this.url, {
+                params: {
+                    api_key: this.api_key,
+                    query: this.title,
+                    language:this.language
+                },
+            })
+                .then(response => {
+                    this.movies = response.data.results;
+                })
         }
     },
     created() {
-        axios.get('https://api.themoviedb.org/3/search/movie?api_key=5c9194b00bb5ab97cca5858e87b81858&query=ritorno+al+futuro')
-                .then((response) => {
-                    this.title = response.data;
-                    console.log(response)
-                })
     }
 }
 </script>
 <!-- My Html -->
 <template>
-    <form>
-        <input type="text">
-        <input type="button" value="Search">
+    <form @submit.prevent="searchMovies">
+        <input type="text" v-model="title">
+        <button type="submit">Search</button>
     </form>
+    <div v-if="movies.length">
+        <p>{{ movies[0].original_title }}</p>
+    </div>
+    <div v-else>
+        <p>Nessun Risultato</p>
+        <p>Prova a cercare quaclos'altro</p>
+    </div>
 </template>
 <!-- My Scss -->
 <style lang="scss" scoped>
