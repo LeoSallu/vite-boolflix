b6489978c7653f975ba31a510036c1c422cef2f8
LeoSallu <L.sallustio99@gmail.com>
2023-03-30 17:24:10 +0200

    funzione per arrotondare voto

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -10,6 +10,11 @@ export default {
             store
         }
     },
+    methods:{
+        vote(element){
+            return Math.ceil( element/ 2);
+        }
+    },
     computed: {
         flag() {
             return (element) => {
@@ -25,30 +30,31 @@ export default {
         }
     }
 }
-
 </script>
 <template>
     <div v-if="store.moviesAndSeries.length">
-        <!-- Ciclo V-for per stampare a schermo le info del film  -->
+        <!-- Ciclo V-for per stampare a schermo le info del film e delle serie -->
         <div v-for="movie in store.moviesAndSeries">
+            <!-- Lista Film  -->   
             <div v-if="movie.title">
                 <h3>Hai cercato : <span>{{movie.title}}</span> .</h3>
                 <ul>
                     <li>Il titolo originale del film è : {{ movie.original_title }}</li>
                     <li>La lingua originale del film è : <country-flag :country=flag(movie.original_language) size='small' /></li>
-                    <li>Il voto della critica del film è : {{ movie.vote_average }}</li>
+                    <li>Il voto della critica del film è : {{ vote(movie.vote_average) }}</li>
                 </ul>  
             </div> 
+            <!-- Lista Serie  -->
             <div v-else-if="movie.name">
                 <h3>Hai cercato : <span>{{movie.name}}</span> .</h3>
                 <ul>
                     <li>Il titolo originale della serie è : {{ movie.original_name }}</li>
                     <li>La lingua originale della serie è : <country-flag :country=flag(movie.original_language) size='small' /></li>
-                    <li>Il voto della critica della serie è : {{ movie.vote_average }}</li>
+                    <li>Il voto della critica della serie è : {{ vote(movie.vote_average) }}</li>
                 </ul>  
             </div>                               
         </div>
-        <!-- /Ciclo V-for per stampare a schermo le info del film  -->
+        <!-- /Ciclo V-for per stampare a schermo le info del film e delle serie -->
     </div>
     <!-- Messaggio in caso di nessun risultato  -->
     <div v-else>
